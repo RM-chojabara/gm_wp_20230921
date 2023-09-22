a45b1d13446bb03e8cc5946e33c860f2f333e917
山本 龍太 ゲスト <yamamoto_ryuta@external.cygames.jp>
2023-09-22 12:32:20 +0900

    cdn コメントアウト

--- header.php
+++ header.php
@@ -248,60 +248,114 @@
 			</div>
 
 			<script type="text/javascript">
-				( async function(){
-					const loginButton = document.getElementById('auth0-login');
-					const logoutButton = document.getElementById('auth0-logout');
-					// console.log('buttons', loginButton, logoutButton);
+				// ( async function(){
+				// 	const loginButton = document.getElementById('auth0-login');
+				// 	const logoutButton = document.getElementById('auth0-logout');
+				// 	// console.log('buttons', loginButton, logoutButton);
 
-					const auth0Client = await auth0.createAuth0Client({
-						domain: "rittor-music-dev.jp.auth0.com",
-						clientId: "YqJvsG9ITMx8duXhLUPHmZj7aUoCt369",
-					});
-					// console.dir(auth0Client);
 
-					// ログイン
-					loginButton.addEventListener('click', (e) => {
-						e.preventDefault();
-						auth0Client.loginWithRedirect({
-							authorizationParams: {
-								redirect_uri: window.location.origin
-							}
-						});
-						console.log('login');
-					});
+				// 	auth0.createAuth0Client({
+				// 		domain: "rittor-music-dev.jp.auth0.com",
+				// 		clientId: "YqJvsG9ITMx8duXhLUPHmZj7aUoCt369",
+				// 		authorizationParams: {
+				// 			redirect_uri: window.location.origin
+				// 		}
+				// 	}).then(async (auth0Client) => {
+				// 		console.dir('auth0Client', auth0Client);
+				// 		// Assumes a button with id "login" in the DOM
+				// 		// const loginButton = document.getElementById("login");
 
-					if (location.search.includes("state=") && (location.search.includes("code=") || location.search.includes("error="))) {
-						await auth0Client.handleRedirectCallback();
-						window.history.replaceState({}, document.title, "/");
-					}
+				// 		loginButton.addEventListener("click", (e) => {
+				// 			e.preventDefault();
+				// 			auth0Client.loginWithRedirect();
+				// 			console.log('login');
+				// 		});
 
-					// ログアウト
-					logoutButton.addEventListener('click', (e) => {
-						e.preventDefault();
-						auth0Client.logout();
-						console.log('logout');
-					});
+				// 		if (location.search.includes("state=") &&
+				// 				(location.search.includes("code=") ||
+				// 				location.search.includes("error="))) {
+				// 			await auth0Client.handleRedirectCallback();
+				// 			window.history.replaceState({}, document.title, "/");
+				// 		}
 
-					// マイページ
-					const isAuthenticated = await auth0Client.isAuthenticated();
-					console.log('isAuthenticated', isAuthenticated);
-					const userProfile = await auth0Client.getUser();
+				// 		// Assumes a button with id "logout" in the DOM
+				// 		// const logoutButton = document.getElementById("logout");
 
-					// Assumes an element with id "profile" in the DOM
-					const profileElement = document.getElementById("profile");
+				// 		logoutButton.addEventListener("click", (e) => {
+				// 			e.preventDefault();
+				// 			auth0Client.logout();
+				// 			console.log('logout');
+				// 		});
 
-					if (isAuthenticated) {
-						profileElement.style.display = "block";
-						profileElement.innerHTML = `
-										<p>${userProfile.name}</p>
-										<img src="${userProfile.picture}" />
-									`;
-						console.log('login now');
-					} else {
-						profileElement.style.display = "none";
-						console.log('not login');
-					}
+				// 		const isAuthenticated = await auth0Client.isAuthenticated();
+				// 		const userProfile = await auth0Client.getUser();
 
-					console.log('auth0 cdk scripts');
-				})();
+				// 		// Assumes an element with id "profile" in the DOM
+				// 		const profileElement = document.getElementById("profile");
+
+				// 		if (isAuthenticated) {
+				// 			profileElement.style.display = "block";
+				// 			profileElement.innerHTML = `
+				// 							<p>${userProfile.name}</p>
+				// 							<img src="${userProfile.picture}" />
+				// 						`;
+
+				// 			console.log('login now');
+				// 		} else {
+				// 			profileElement.style.display = "none";
+				// 			console.log('not login');
+				// 		}
+				// 	});
+
+				// 	// const auth0Client = await auth0.createAuth0Client({
+				// 	// 	domain: "rittor-music-dev.jp.auth0.com",
+				// 	// 	clientId: "YqJvsG9ITMx8duXhLUPHmZj7aUoCt369",
+				// 	// });
+				// 	// // console.dir(auth0Client);
+
+				// 	// // ログイン
+				// 	// loginButton.addEventListener('click', (e) => {
+				// 	// 	e.preventDefault();
+				// 	// 	auth0Client.loginWithRedirect({
+				// 	// 		authorizationParams: {
+				// 	// 			redirect_uri: window.location.origin
+				// 	// 		}
+				// 	// 	});
+				// 	// 	console.log('login');
+				// 	// });
+
+				// 	// if (location.search.includes("state=") && (location.search.includes("code=") || location.search.includes("error="))) {
+				// 	// 	await auth0Client.handleRedirectCallback();
+				// 	// 	window.history.replaceState({}, document.title, "/");
+				// 	// }
+
+				// 	// // ログアウト
+				// 	// logoutButton.addEventListener('click', (e) => {
+				// 	// 	e.preventDefault();
+				// 	// 	auth0Client.logout();
+				// 	// 	console.log('logout');
+				// 	// });
+
+				// 	// // マイページ
+				// 	// const isAuthenticated = await auth0Client.isAuthenticated();
+				// 	// console.log('isAuthenticated', isAuthenticated);
+				// 	// const userProfile = await auth0Client.getUser();
+
+				// 	// // Assumes an element with id "profile" in the DOM
+				// 	// const profileElement = document.getElementById("profile");
+
+				// 	// if (isAuthenticated) {
+				// 	// 	profileElement.style.display = "block";
+				// 	// 	profileElement.innerHTML = `
+				// 	// 					<p>${userProfile.name}</p>
+				// 	// 					<img src="${userProfile.picture}" />
+				// 	// 				`;
+				// 	// 	console.log('login now');
+				// 	// } else {
+				// 	// 	profileElement.style.display = "none";
+				// 	// 	console.log('not login');
+				// 	// }
+
+				// 	console.log('auth0 cdk scripts');
+				// })();
 			</script>
